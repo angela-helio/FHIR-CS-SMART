b8e18896410b18cd2ed232ecc52c3a59606bcd5e
Doug <doug@heliossoftware.com>
2025-08-29 18:19:36 -0400

    feat: add lombok, change secrets

--- src/main/java/com/example/smartspring/config/AppProperties.java
+++ src/main/java/com/example/smartspring/config/AppProperties.java
@@ -1,17 +1,17 @@
 package com.example.smartspring.config;
+
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+@Data
 @Component
-@ConfigurationProperties(prefix="smart")
+@ConfigurationProperties(prefix = "smart")
 public class AppProperties {
   private String fhirBase;
   private String clientId;
-  private String redirectUri="http://127.0.0.1:8080/callback";
-  private String scopes="launch/patient patient.read openid fhirUser offline_access";
+  private String clientSecret;
+  private String redirectUri = "http://127.0.0.1:8080/callback";
+  private String scopes = "launch/patient patient.read openid fhirUser offline_access";
   private String launch; // optional for standalone EHR-like launch
-  public String getFhirBase(){return fhirBase;} public void setFhirBase(String v){this.fhirBase=v;}
-  public String getClientId(){return clientId;} public void setClientId(String v){this.clientId=v;}
-  public String getRedirectUri(){return redirectUri;} public void setRedirectUri(String v){this.redirectUri=v;}
-  public String getScopes(){return scopes;} public void setScopes(String v){this.scopes=v;}
-  public String getLaunch(){return launch;} public void setLaunch(String v){this.launch=v;}
 }
